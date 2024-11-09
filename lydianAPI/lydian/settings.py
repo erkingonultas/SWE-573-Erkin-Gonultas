28df216dea0c54862b43665bb12dadf500fcd134
erkingonultas <erkingonultas@gmail.com>
2024-11-10 01:40:14 +0300

    media try again

--- lydianAPI/lydian/settings.py
+++ lydianAPI/lydian/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/post_images'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/post_images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/post_images')
