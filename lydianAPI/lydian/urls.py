594423487f086271de34a255db897420a593556a
erkingonultas <erkingonultas@gmail.com>
2024-11-10 01:32:44 +0300

    media root fix maybe

--- lydianAPI/lydian/urls.py
+++ lydianAPI/lydian/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path('', include('users.urls')),
     ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Configure Admin Title
 admin.site.site_header = "Lydian Admin"
