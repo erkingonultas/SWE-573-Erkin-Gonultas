bdc9ee97cadd90c6567f14e0e9848c7b727dcbb3
erkingonultas <erkingonultas@gmail.com>
2024-11-13 22:57:51 +0300

    post delete function and full page post implemented

--- lydianAPI/users/urls.py
+++ lydianAPI/users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('post/', views.create_post, name='create_post'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
 ]
